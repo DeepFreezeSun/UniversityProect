931b597e569d972a91b8910e7219835d13e03950
DeepFreezeSun <PinguinoWr@gmail.com>
2015-02-26 00:47:35 +0300

    whatever

---
+++ UniversityProect/UniversityProect.DesktopClient/Screens/CreateNewClient.lsml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class CreateNewClient
+    {
+        partial void CreateNewClient_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
+        {
+            // Write your code here.
+            this.ClientProperty = new Client();
+        }
+
+        partial void CreateNewClient_Saved()
+        {
+            // Write your code here.
+            this.Close(false);
+            Application.Current.ShowDefaultScreen(this.ClientProperty);
+        }
+    }
+}
